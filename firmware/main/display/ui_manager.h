60c02bf682b128ea5b8c9aae9d2d09b87be186e7
root <root@coding.localdomain>
2026-06-23 12:25:24 +0100

    Add proper provisioning screen with mixed font sizes for clean layout

--- firmware/main/display/ui_manager.h
+++ firmware/main/display/ui_manager.h
@@ -11,6 +11,7 @@ void ui_show_processing_screen(void);
 void ui_update_processing_anim(int frame);
 void ui_show_response(const char *text);
 void ui_show_error(const char *message);
+void ui_show_provisioning_screen(const char *ap_name, const char *url);
 void ui_show_sleep_screen(void);
 void ui_show_docked_screen(void);
 void ui_show_driving_screen(void);
